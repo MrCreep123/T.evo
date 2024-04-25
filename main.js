c2a1df7c26895961b0bacbbbaa1713d915071c05
MrCreep123 <83638023+MrCreep123@users.noreply.github.com>
2024-04-25 17:28:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+    arrows: false,
+    autoplay: true,
+    infinite: true
+    });
+    });
+    const cursorSmall = document.querySelector('.small');
+    const cursorBig = document.querySelector('.big');
+    
+    
+    const positionElement = (e)=> {
+      const mouseY = e.clientY;
+      const mouseX = e.clientX;
+       
+      cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+      
+      cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+     
+    }
+    
+    window.addEventListener('mousemove', positionElement)
---
+++ what.js
@@ -0,0 +1,1 @@
+document.getElementById("ent_log").value = "MrCreep";
